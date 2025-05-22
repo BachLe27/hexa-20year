c24fd71d1d7f9cd77b40d7d52f2e13a646f03c26
BachLe27 <bachlamsung123@gmail.com>
2025-05-22 09:58:46 +0700

    bugfix: video url

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "@mui/material"
-import video from '../assets/0522.mp4'
 import { useRef, useState } from "react";
 
 const Videos = ({ nextStep }: { nextStep: () => void }) => {
@@ -14,20 +13,20 @@ const Videos = ({ nextStep }: { nextStep: () => void }) => {
   }
 
   return (
-    <Stack className='z-100' alignItems='center' justifyContent='center'>
+    <Stack className='z-150' alignItems='center' justifyContent='center'>
       <div style={{
         opacity: opacity,
         transition: 'opacity 1s ease-in-out',
         width: '100%',
         height: '100vh'
       }}>
-        <video onEnded={handleEnded} ref={videoRef} className="z-50 h-[100vh] w-full" autoPlay>
-          <source src={video} type="video/mp4" />
+        <video onEnded={handleEnded} ref={videoRef} className="z-150 h-[100vh] w-full" autoPlay>
+          <source src='/0522.mp4' type="video/mp4" />
           Your browser does not support HTML video.
         </video>
       </div>
 
-      <button className='border-2 border-white text-white px-4 py-2 rounded-md absolute bottom-10 right-10 cursor-pointer z-51' onClick={nextStep}>Bỏ qua</button>
+      <button className='border-2 border-white text-white px-4 py-2 rounded-md absolute bottom-10 right-10 cursor-pointer z-151' onClick={nextStep}>Bỏ qua</button>
     </Stack>
   )
 }
